7c83be71c1e35b1183e9bba4b532068c83fdb8b7
HayGrouve <haygrouve@gmail.com>
2020-03-01 16:14:19 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
+//Run app, then load http://localhost:3000 in a browser to see the output.
