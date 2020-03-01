86bac3530d4f2c3c9c06d3c532a9c181f697da5c
HayGrouve <haygrouve@gmail.com>
2020-03-01 19:32:04 +0200

    setting up backend logic

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
-const express = require('express');
-const app = express();
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
+    methodOverride = require("method-override");
 
+//REQUIRING ROUTES
+var indexRoutes = require("./routes/index");
 
+var url = process.env.DATABASEURL || "mongodb://localhost/tsf";
 var port = process.env.PORT || 3000;
-
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-    res.render('landing');
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to DB!');
+}).catch(err => {
+    console.log('ERROR:', err.message);
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.set('view engine', 'ejs');
+mongoose.set('useCreateIndex', true);
+
+//PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "This is secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    app.locals.moment = require('moment');
+    next();
+});
+
+app.use(indexRoutes);
+
 
 app.get('/home', (req, res) => {
     res.render('home');
@@ -33,10 +75,10 @@ app.get('/hockey', (req, res) => {
     res.render("hockey");
 });
 
-app.listen(port, (err)=>{
-    if(err){
+app.listen(port, (err) => {
+    if (err) {
         console.log(`Error: ${err.message}`)
-    }else{
+    } else {
         console.log(`App running on port ${port}`);
     }
 });
